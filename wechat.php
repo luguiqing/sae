61503e595845ec51998275e8bf45a49f0e9ce593
LuGuiQing <1466594175@qq.com>
2016-08-13 02:18:55 +0800

    ..

--- wechat.php
+++ wechat.php
@@ -237,7 +237,7 @@ $signPackage = $jssdk->GetSignPackage();
                                         localId: localId1 
                                     });
                                     var index = $('.wechat>div:last').index()+1;
-                                    $("footer").before("<div class='right box'><div class='right_child' alt='头像'><i class='iconfont' style='color:blue'>&#xe65d;</i><img src='img/1.jpg' style='margin-left:10px;margin-bottom:-12px'/></div><audio controls='controls' id="+index+"><source src="+localId1+"type='audio/mpeg'/></audio></div>");
+                                    $("footer").before("<div class='right box'><div class='right_child' alt='头像'><i class='iconfont' style='color:blue'>&#xe65d;</i><img src='img/1.jpg' style='margin-left:10px;margin-bottom:-12px'/></div><audio controls='controls' id="+index+"><source type='audio/mpeg' src="+localId1+"/></audio></div>");
                                 },
                                 fail:function(){
                                     alert("下载失败！");
