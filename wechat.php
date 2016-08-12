2be83626dd27c6f315fc4ec1cac35f85451d303b
luguiqing <luguiqing1234@qq.com>
2016-08-12 10:14:41 +0800

    ...

--- wechat.php
+++ wechat.php
@@ -208,6 +208,7 @@ $signPackage = $jssdk->GetSignPackage();
         $("#sendtxtbtn").on("click",function(){
             alert("点击了按钮");
             if($(".text_message input").val()){
+                alert($(".text_message input").val());
                 $(".wechat div:last").after("<div>"+$(".text_message input").val()+"</div>");
             }else{
                 alert("发送的信息不能为空！");
